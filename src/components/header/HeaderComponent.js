710aa775e5d582e9ad9329a4ebb7937eef0926b0
Agustín Lucentini <136536384+lucenstuff@users.noreply.github.com>
2023-10-04 22:21:56 -0300

    added responsive navbar component

---
+++ src/components/header/HeaderComponent.js
@@ -0,0 +1,33 @@
+class HeaderComponent extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    fetch('src/components/header/header-component.html')
+      .then(response => response.text())
+      .then(content => {
+        this.innerHTML = content;
+
+        const burgerMenu = this.querySelector(".burger-menu");
+
+        burgerMenu.addEventListener("click", () => {
+          this.toggleMenu();
+        });
+      })
+      .catch(error => {
+        console.error('Error al cargar el archivo HTML:', error);
+      });
+  }
+
+  toggleMenu() {
+    const navMenu = this.querySelector(".nav-links");
+
+    navMenu.classList.toggle("active");
+
+    const burgerMenuIcon = this.querySelector(".burger-menu");
+    burgerMenuIcon.classList.toggle("active");
+  }
+}
+
+window.customElements.define('header-component', HeaderComponent);
