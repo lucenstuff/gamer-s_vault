01cec667f50249b93639418781c6fa80ed827a15
Agustín Lucentini <136536384+lucenstuff@users.noreply.github.com>
2023-10-05 17:30:57 -0300

    improvements to the slider

--- src/components/img-slider/SliderComponent.js
+++ src/components/img-slider/SliderComponent.js
@@ -3,16 +3,22 @@ class SliderComponent extends HTMLElement {
     super();
   }
 
-  connectedCallback() {
-    fetch('src/components/img-slider/slider-component.html')
-      .then(response => response.text())
-      .then(content => {
-        this.innerHTML = content;
-      })
-      .catch(error => {
-        console.error('Error al cargar el archivo HTML:', error);
-      });
+  async connectedCallback() {
+    try {
+      const response = await fetch('src/components/img-slider/slider-component.html');
+
+      if (!response.ok) {
+        throw new Error(`Error loading HTML file: ${response.statusText}`);
+      }
+
+      const content = await response.text();
+      this.innerHTML = content;
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
 }
+
+
 
 window.customElements.define('slider-component', SliderComponent);
