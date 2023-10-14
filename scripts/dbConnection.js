a0b77bcb7cb302956f60277a123296d0f5ce7fea
Agustín Lucentini <136536384+lucenstuff@users.noreply.github.com>
2023-10-14 18:15:39 -0300

    Implementing db

--- scripts/dbConnection.js
+++ scripts/dbConnection.js
@@ -1,24 +1,31 @@
-let mysql = require('mysql2');
+const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'admin',
-    database: 'ecommerce_test'
-})
+  host: 'localhost',
+  user: 'root',
+  password: 'admin',
+  database: 'ecommerce_test'
+});
 
 connection.connect((error) => {
-    if(error){
-        console.log('Error connecting to the database')
-        return;
-    }
-    console.log('Successfully connected to the database.');
-    
-    connection.end((error) => {
-        if (error) {
-            console.log('Error closing the database connection');
-            return;
-        }
-        console.log('Closed the database connection')
-    })
+  if (error) {
+    console.log('Error connecting to the database');
+    return;
+  }
+  console.log('Successfully connected to the database.');
+
+});
+
+
+
+const users = "SELECT * FROM Products";  
+
+connection.query(users, (error, results) => {
+  if (error) {
+    console.log('Error executing query:', error);
+    return;
+  }
+  console.log(results);
 })
+
+connection.end();
