2daf5fad35c93feade577197cba5d7ee0dedb6fe
Agustín Lucentini <136536384+lucenstuff@users.noreply.github.com>
2023-10-17 23:05:59 -0300

    deleted ejsTest.js

--- public/src/components/game-card/GameCard.js
+++ public/src/components/game-card/GameCard.js
@@ -1,23 +1,21 @@
+// game-card.js
 class GameCard extends HTMLElement {
   constructor() {
     super();
   }
 
   connectedCallback() {
-    // Fetch and render data for each game-card based on the provided data-product-id attribute
     const productId = this.getAttribute('data-product-id');
     if (productId) {
       this.fetchAndRenderGameData(productId);
-      this.loadGameCardCSS(); // Load the CSS for the game card
+      this.loadGameCardCSS();
     }
   }
 
   fetchAndRenderGameData(productId) {
-    // Fetch data from your /getGameData endpoint on your server
     fetch('/getGameData')
       .then(response => response.json())
       .then(gameData => {
-        // Filter the game data based on the product ID
         const productData = gameData.find(product => product.ProductID === parseInt(productId));
 
         if (productData) {
@@ -32,57 +30,26 @@ class GameCard extends HTMLElement {
   }
 
   renderGameCard(data) {
-    // Create a product card element
-    const productCard = document.createElement('div');
-    productCard.classList.add('product-card');
+    fetch('src/components/game-card/game-card.html')
+      .then(response => response.text())
+      .then(html => {
+        const template = document.createElement('template');
+        template.innerHTML = html.trim();
+        const content = template.content.cloneNode(true);
+        const image = content.querySelector('.product-card img');
+        const title = content.querySelector('.product-card h3');
+        const price = content.querySelector('.product-card .price');
 
-    // Create an image element
-    const image = document.createElement('img');
-    image.src = data.ImageURL;
-    image.alt = data.ProductName;
+        image.src = data.ImageURL;
+        image.alt = data.ProductName;
+        title.textContent = data.ProductName;
+        price.textContent = `$${data.Price}`;
 
-    // Create an element for product title
-    const title = document.createElement('h3');
-    title.textContent = data.ProductName;
-
-    // Create an element for product price
-    const price = document.createElement('p');
-    price.textContent = `$${data.Price}`;
-
-    // Create a div for card buttons
-    const cardButtons = document.createElement('div');
-    cardButtons.classList.add('card-buttons');
-
-    // Create a favorite button
-    const favoriteButton = document.createElement('a');
-    favoriteButton.href = '#';
-    favoriteButton.innerHTML = '<i class="material-icons heart">favorite</i>';
-
-    // Create a cart button
-    const cartButton = document.createElement('a');
-    cartButton.href = '#';
-    cartButton.innerHTML = '<i class="material-icons cart">shopping_cart</i>';
-
-    // Append elements to the product card
-    productCard.appendChild(image);
-    productCard.appendChild(title);
-    productCard.appendChild(price);
-    cardButtons.appendChild(favoriteButton);
-    cardButtons.appendChild(cartButton);
-    productCard.appendChild(cardButtons);
-
-    // Append the product card to the custom element (game-card)
-    this.appendChild(productCard);
-  }
-
-  loadGameCardCSS() {
-    // Create a link element for the CSS file
-    const linkElement = document.createElement('link');
-    linkElement.rel = 'stylesheet';
-    linkElement.href = 'src/components/game-card/gameCard.css';
-
-    // Append the link element to the document's head
-    document.head.appendChild(linkElement);
+        this.appendChild(content);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 }
 
