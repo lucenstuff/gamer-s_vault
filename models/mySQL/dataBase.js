15ca89c43e9e8a54b4450ee11a4fffc5c5a06811
lucenstuff <lucentiniagustin@hotmail.com>
2023-12-07 16:50:05 -0300

    better loading times

--- models/mySQL/dataBase.js
+++ models/mySQL/dataBase.js
@@ -1,28 +1,16 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE,
-//   port: process.env.DB_PORT,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
 const pool = mysql.createPool({
-  host: "aws.connect.psdb.cloud",
-  user: "8fl8jvnulx12frrixg6w",
-  password: "pscale_pw_p28fqQxblMERGM5m9f2K8GVu0dyLjtw7d5iPspW8nWY",
-  database: "gamers_vault",
-  port: "3306",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
-
 async function runQuery(query, callback) {
   pool.getConnection((err, connection) => {
     if (err) {
