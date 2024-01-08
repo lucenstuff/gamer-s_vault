129c3b6f5c628f4674b4fb4fe697580a189d3e67
lucenstuff <lucentiniagustin@hotmail.com>
2024-01-08 12:50:04 -0300

    cors not working why

--- back/server.js
+++ back/server.js
@@ -8,30 +8,10 @@ import { MercadoPagoConfig, Payment, Preference } from "mercadopago";
 import dotenv from "dotenv";
 dotenv.config();
 
-const ipAdress = "localhost";
+const ipAdress = "localhost" || "0.0.0.0";
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT;
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://gamersvault.onrender.com/"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
-  next();
-});
 
 //MercadoPago
 const client = new MercadoPagoConfig({
--- front/utils/apiConnection.js
+++ front/utils/apiConnection.js
@@ -1,4 +1,4 @@
-// const apiUrl = "http://localhost:8080/api";
+// const apiUrl = "http://localhost:3000/api";
 const apiUrl = "https://gamersvaultbackend.onrender.com/api";
 
 async function getProducts() {
