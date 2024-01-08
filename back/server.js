85c088480091084187bfd7205d1ce64a0fb9461e
lucenstuff <lucentiniagustin@hotmail.com>
2024-01-08 11:54:27 -0300

    added cors for render

--- back/server.js
+++ back/server.js
@@ -13,7 +13,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 app.use(
   cors({
-    origin: "http://127.0.0.1:5500",
+    origin: ["http://127.0.0.1:5500", "https://gamersvault.onrender.com"],
   })
 );
 
